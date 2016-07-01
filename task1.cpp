56ea83269a6be45d09b7472974d48b311c9bc514
setlol <dovnar.maxim@gmail.com>
2016-07-01 22:49:18 +0300

    homework

---
+++ task1.cpp
@@ -0,0 +1,26 @@
+﻿#include <locale.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int number, count, zero, sum;
+
+	cout << "определяем количество цифр и нулей в этом числе в этом числе, посчитаем их сумму и среднее арифметическое" "\n"
+		"введите число ";
+	cin >> number;
+
+	for (count = 0, zero = 0, sum = 0; number > 0; number /= 10, count++)
+	{
+		int divisionRemainder = number % 10;
+		if (divisionRemainder == 0)
+		{
+			zero++;
+		}
+		sum += divisionRemainder;
+	}
+	cout << "в этом числе в этом числе = " << count << " чисел и " << zero << " нулей" << endl
+		<< "сумма цыфр этого числа = " << sum << " и среднее арифметическое = " << sum*1.0 / count << endl;
+}
---
+++ task2.cpp
@@ -0,0 +1,43 @@
+﻿#include <locale.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int cellSize, quantityСell, line, symbol, countSymbol;
+	cout << "размер клетки от 1 до 9 = ";
+	cin >> cellSize;
+	cout << "количество клеток от 2 до 8 = ";
+	cin >> quantityСell;
+	countSymbol = cellSize * quantityСell;
+	if (cellSize < 1 ||
+		cellSize > 9 ||
+		quantityСell < 2 ||
+		quantityСell > 8)
+	{
+		cout << "вы ввели не верные данные, проверьте ввод!!";
+	}
+	for (line = 0; line < countSymbol; line++)
+	{
+		for (symbol = 0; symbol < countSymbol; symbol++)
+		{
+			if ((line / cellSize) % 2 == 0 &&
+				(symbol / cellSize) % 2 == 0)
+			{
+				cout << '*';
+			}
+			else if ((line / cellSize) % 2 == 1 &&
+				(symbol / cellSize) % 2 == 1)
+			{
+				cout << '*';
+			}
+			else
+			{
+				cout << '-';
+			}
+		}
+		cout << endl;
+	}
+}
